697d9f32d26717597ede8daeb0ad280828dd5fb4
mishab777 <muhammedmishab002@gmail.com>
2024-12-27 11:39:23 +0530

    initial commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css" 
 import logo from "./lh-logo-2.png"
 import {Link } from 'react-router-dom';
@@ -6,12 +6,22 @@ import { AiOutlineShopping } from "react-icons/ai";
 import { IoPersonOutline } from "react-icons/io5";
 import { FiSearch } from "react-icons/fi";
 import { GrMenu } from "react-icons/gr";
+import { IoCloseCircle } from "react-icons/io5";
+
+
 
 const Navbar = () => {
+
+  const [isOffCanvasOpen, setIsOffCanvasOpen] = useState(false);
+
+  const toggleOffCanvas = () => {
+    setIsOffCanvasOpen(!isOffCanvasOpen);
+  };
+
   return (
   <>
   <div className='flex flex-row p-3 md:justify-evenly justify-between items-center h-20'>
-    <div className='flex items-center justify-center gap-2'>
+    <div className='flex items-center justify-center gap-2 cursor-pointer' onClick={toggleOffCanvas}>
     <GrMenu size={20}/>
     <span>MENU</span>
     </div>
@@ -24,6 +34,30 @@ const Navbar = () => {
       <button><AiOutlineShopping size={24}/></button>
     </div>
   </div>
+  <div 
+        className={`fixed top-0 left-0 w-80 md:w-96 h-full bg-white z-50 border-r border-gray-300 p-4 transform ${
+          isOffCanvasOpen ? 'translate-x-0' : '-translate-x-full'
+        } transition-transform duration-300 ease-in-out`}
+      >
+        <button 
+          onClick={toggleOffCanvas} 
+          className="absolute top-4 right-4 border-0"
+        >
+          <IoCloseCircle size={25}/>
+        </button>
+        <nav className="mt-8">
+          <img src={logo} className='w-auto h-14 object-contain mb-4' alt="" />
+          <ul className="space-y-4">
+            <li className='text-black text-xl'><Link to="/" onClick={toggleOffCanvas}>Perfumes</Link></li>
+            <li className='text-black text-xl'><Link to="/" onClick={toggleOffCanvas}>Bags</Link></li>
+            <li className='text-black text-xl'><Link to="/" onClick={toggleOffCanvas}>Wallets</Link></li>
+            <li className='text-black text-xl'><Link to="/" onClick={toggleOffCanvas}>Sandals</Link></li>
+            <li className='text-black text-xl'><Link to="/" onClick={toggleOffCanvas}>Watches</Link></li>
+            <li className='text-black text-xl'><Link to="/" onClick={toggleOffCanvas}>About us</Link></li>
+            <li className='text-black text-xl'><Link to="/" onClick={toggleOffCanvas}>Contact us</Link></li>
+          </ul>
+        </nav>
+      </div>
   </>
   )
 }
